b85693bab7236d7791bc6d891a6b83ab2dee97ca
Usama Rehan Farooqi <109687592+URF005@users.noreply.github.com>
2024-08-28 21:45:58 +0500

    Update hardhat.config.js

--- REALESTATE/web3/hardhat.config.js
+++ REALESTATE/web3/hardhat.config.js
@@ -1,4 +1,4 @@
-const PRIVATE_KEY = "hdhbhdcb788bfb3bn3nk3idnd3iddn";
+const PRIVATE_KEY = "hdhbhdcb767fbfnjdf893nkdmnd3iddn";
 const RPC_URL = "https://rpc.ankr.com/polygon_amoy";
 module.exports = {
   defaultNetwork: "polygon",
